e396ebc45a21350138a00cea69e425ef02813800
Vikas <dhimanv209@gmail.com>
2026-07-29 23:19:08 +0530

    chore: initial Android Studio Compose scaffold
    Starting point for the Cars24 SDUI assignment: default Empty Activity
    template (Compose, Material3, Kotlin 2.0.21, AGP 9.1.0).

---
+++ app/src/main/java/com/vikas/cars24sdui/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.vikas.cars24sdui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vikas.cars24sdui.ui.theme.Cars24SDUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Cars24SDUITheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Cars24SDUITheme {
+        Greeting("Android")
+    }
+}
